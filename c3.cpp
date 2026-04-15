a4d1335da78d61d4b0588b1364aeaf40fa177260
Shreya <yshreya178@gmail.com>
2026-04-15 20:16:56 +0530

    Add files via upload

--- c3.cpp
+++ c3.cpp
@@ -1,29 +1,14 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
-    //Nested if statement
-     int age;
-    cin>>age;
- if (age>22){
- cout<<"Adult"<<endl;
+vector<vector<int>>matt={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+
+for(int i=0;i<matt.size();i++){
+    for(int j=0;j<matt[i].size();j++){
+        cout<<matt[i][j]<<" ";
+    }
+    cout<<endl;
 }
-    else if (age>22){
-    cout<<"Young Generation"<<endl;
+return 0;
 }
-   else if (age>18){
-  cout<<"Teenager"<<endl;
-}
-   else if(age>12){
-    cout<<"Child"<<endl;
-}
-   else if (age>5){
-    cout<<"Baby"<<endl;
-}
- else 
-     
-   { cout<<"infant"<<endl;
- }
- 
-  return 0;
-  }
-  
